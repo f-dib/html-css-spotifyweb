f6f57d791fbb5e4b62a41f2fdfa0bd35def841da
federico-dibitonto <f.dibitonto94@gmail.com>
2024-01-15 17:03:53 +0100

    exercise completed + bonus + extra (query 477px with different player, gradient background, scrollbar style, hover on volume + fix scrollbar main-body, added time animation) + minor fix

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-const element = document.querySelector(".myBtn");
+const element = document.getElementById("myBtn");
 element.addEventListener("click", function() {
     document.querySelector(".timer").style.width = "100%";
+    document.getElementById("myBtn").classList.toggle('fa-circle-play');
+    document.getElementById("myBtn").classList.toggle('fa-circle-pause');
 });
 
-const celltime = document.querySelector(".myBtn-j");
+const celltime = document.getElementById("myBtn-j");
 celltime.addEventListener("click", function() {
     document.querySelector(".timer-j").style.width = "100%";
+    document.getElementById("myBtn-j").classList.toggle('fa-circle-play');
+    document.getElementById("myBtn-j").classList.toggle('fa-circle-pause');
 });
