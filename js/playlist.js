4973f973b120a105b85170fcdfbe6e952403d485
federico-dibitonto <f.dibitonto94@gmail.com>
2024-07-04 21:10:03 +0200

    minor fix style and create playlist for new features

---
+++ js/playlist.js
@@ -0,0 +1,8 @@
+let songs = [
+    {
+        name : "IO, ME ED ALTRI GUAI",
+        artist : "Rose Villain",
+        img : "RadioSakura.jpg",
+        audio : "IO, ME ED ALTRI GUAI.mp3"
+    }
+]
