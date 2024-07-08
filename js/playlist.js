4629aa7b94febd4af9b14000e77b31dd8bb4e5e9
federico-dibitonto <f.dibitonto94@gmail.com>
2024-07-09 01:48:05 +0200

    first changes to the player

--- js/playlist.js
+++
@@ -1,8 +0,0 @@
-let songs = [
-    {
-        name : "IO, ME ED ALTRI GUAI",
-        artist : "Rose Villain",
-        img : "RadioSakura.jpg",
-        audio : "IO, ME ED ALTRI GUAI.mp3"
-    }
-]
